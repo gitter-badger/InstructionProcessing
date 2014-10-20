4b2007a742c57ffc4f13da5f06a8a7a987e8dfc3
Adnan Gazi <adnan.gazi01@gmail.com>
2014-10-20 14:37:11 +0100

    moved validation into InstructionMessage constructor so can be accessed with default add() method

--- src/main/java/io/github/adnang/InstructionProcessing/InstructionMessage.java
+++ src/main/java/io/github/adnang/InstructionProcessing/InstructionMessage.java
@@ -11,7 +11,17 @@ public class InstructionMessage {
             uom,
             timeStamp;
 
-    public InstructionMessage(int instructionType, int productCode, int quantity, int uom, int timeStamp) {
+    public InstructionMessage(int instructionType, int productCode, int quantity, int uom, int timeStamp)
+        throws InvalidMessageException
+    {
+
+        if (instructionType <= 0 || instructionType >= 100)
+            throw new InvalidMessageException();
+        if (productCode <= 0 || quantity <= 0 || timeStamp <= 0)
+            throw new InvalidMessageException();
+        if (uom < 0 || uom >= 256)
+            throw new InvalidMessageException();
+
         this.instructionType = instructionType;
         this.productCode = productCode;
         this.quantity = quantity;
--- src/main/java/io/github/adnang/InstructionProcessing/InstructionQueue.java
+++ src/main/java/io/github/adnang/InstructionProcessing/InstructionQueue.java
@@ -25,17 +25,13 @@ public class InstructionQueue extends PriorityQueue<InstructionMessage>{
     public void addInstruction(int instructionType, int productCode, int quantity, int uom, int timeStamp)
             throws InvalidMessageException
     {
-
-        if (instructionType <= 0 || instructionType >= 100) throw new InvalidMessageException();
-        if (productCode <= 0 || quantity <= 0 || timeStamp <= 0)
-            throw new InvalidMessageException();
-        if (uom < 0 || uom >= 256) throw new InvalidMessageException();
-
         add(new InstructionMessage(
-                instructionType,
-                productCode,
-                quantity,
-                uom,
-                timeStamp));
+                    instructionType,
+                    productCode,
+                    quantity,
+                    uom,
+                    timeStamp
+                )
+        );
     }
 }
